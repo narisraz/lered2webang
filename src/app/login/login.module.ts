87ab06cc38e4488544b2adf8fb16aff131531792
narisraz <naris.razafimahatratra@zoho.com>
2021-10-20 11:22:47 +0300

    Finished login view

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,20 +8,22 @@ import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule,
-    TemplateModule,
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule
-  ]
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+        TemplateModule,
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule
+    ]
 })
 export class LoginModule { }
