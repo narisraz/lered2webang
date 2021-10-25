0117f8b99fe9dff7e49ee76eeb699d250505295d
narisraz <naris.razafimahatratra@zoho.com>
2021-10-25 11:25:03 +0300

    Add angularFire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'redaction-8c584',
+    appId: '1:129871292191:web:85bffb8281a4b4af78322a',
+    storageBucket: 'redaction-8c584.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAwDGdjX6C4IMaqfSk7HE2ikfo_hh25mIA',
+    authDomain: 'redaction-8c584.firebaseapp.com',
+    messagingSenderId: '129871292191',
+  },
   production: false
 };
 
